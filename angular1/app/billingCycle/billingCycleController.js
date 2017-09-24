772dd81a32b8065d3bb4e070304da8a7e437c281
jaopsb <jpsbdbz@gmail.com>
2017-09-24 20:47:25 -0300

    atualizando repo erro paginacao

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,20 +1,28 @@
 (function() {
   angular.module('primeiraApp').controller('BillingCycleCtrl',[
     '$http',
+    '$location',
     'msgs',
     'tabs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http,msgs,tabs) {
+  function BillingCycleController($http,$location,msgs,tabs) {
     const vm = this
     const url = 'http://localhost:3003/api/billingCycles'
 
     vm.refresh = function(){ // funcao que atualiza a lista com a resposta do servidor
-      $http.get(url).then(function(response){
+      const page = parseInt($location.search().page) || 1
+      $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response){//pesquisa pelo noderestful para limitar em 10 e pular os n*10 registros
         vm.billingCycle = {credits: [{}],debts: [{}]}
         vm.billingCycles = response.data
-        tabs.show(vm,{tabList: true, tabCreate: true})
+        vm.calculateValues()
+
+        $http.get(`${url}/count`).then(function(response) {
+          vm.pages = Math.ceil(response.data.value /10)
+          tabs.show(vm,{tabList: true, tabCreate: true})
+
+        })
       })
     }
 
@@ -29,11 +37,13 @@
 
     vm.showTabUpdate = function(billingCycle) { // funcao que deixa disponivel a tab tabUpdate
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm,{tabUpdate:true})
     }
 
     vm.showTabDelete = function(billingCycle) { // funcao que deixa disponivel a tab tabUpdate
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm,{tabDelete:true})
     }
 
@@ -59,30 +69,52 @@
 
     vm.addCredit = function (index) {
       vm.billingCycle.credits.splice(index + 1, 0, {})
+      vm.calculateValues()
     }
 
     vm.cloneCredit = function (index,{name, value}) {
       vm.billingCycle.credits.splice(index + 1, 0, {name,value})
+      vm.calculateValues()
     }
 
     vm.deleteCredit = function(index) {
       if(vm.billingCycle.credits.length > 1){
         vm.billingCycle.credits.splice(index,1)
+        vm.calculateValues()
       }
     }
 
     vm.addDebt = function(index) {
       vm.billingCycle.debts.splice(index + 1, 0, {})
+      vm.calculateValues()
     }
 
     vm.cloneDebt = function(index,{name,value}) {
       vm.billingCycle.debts.splice(index + 1, 0, {name,value})
+      vm.calculateValues()
     }
 
     vm.deleteDebt = function(index) {
       if(vm.billingCycle.debts.length > 1){
         vm.billingCycle.debts.splice(index,1)
+        vm.calculateValues()
       }
+    }
+
+    vm.calculateValues = function() {
+      vm.credit = 0
+      vm.debt = 0
+
+      if(vm.billingCycle){
+        vm.billingCycle.credits.forEach(function({value}) {
+          vm.credit += !value || isNaN(value) ? 0 : parseFloat(value) //caso value exista e for um numero faz-se um parse e soma a credit senao passa 0
+        })
+
+        vm.billingCycle.debts.forEach(function({value}) {
+          vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+      }
+      vm.total = vm.credit - vm.debt
     }
 
     vm.refresh() //toda vez que for chamada a funcao principal,acaba com um refresh da lista
--- backend/config/server.js
+++ backend/config/server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser')
 const express    = require('express')
 const server     = express()
 const allowCors  = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended:true })) //receber uma url com dados mais extendida do que a padrao
 server.use(bodyParser.json()) //receber tudo que for json diretamente no formato padrao json
 server.use(allowCors)//Cors para crossplatform request, para que a app do angular possa chamar o BACKEND e outras app falarem com BACKEND
+server.use(queryParser())
 
 server.listen(port,function () {
   console.log(`BACKEND is running on port ${port}.`);
